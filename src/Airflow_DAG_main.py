bd6b5b656c258b2b8310a77a4615163fb23db427
prathameshk30 <prathamesh.kulkarni@okstate.edu>
2023-04-04 10:56:49 -0500

    update main

--- src/Airflow_DAG_main.py
+++ src/Airflow_DAG_main.py
@@ -6,6 +6,3 @@ from airflow.models import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 from includes.vs_modules.test import hello
-
-
-from 
